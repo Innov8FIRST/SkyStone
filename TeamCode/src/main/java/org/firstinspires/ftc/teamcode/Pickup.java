efed481fef9b67296a28a18b0b3644e772894ad2
maya <purplecoolness101@gmail.com>
2019-09-22 15:34:06 -0400

    stubbed out pickup methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pickup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pickup.java
@@ -4,8 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Pickup {
-    Telemetry telemetry = new Telemetry();
-    public void pull() {
-        telemetry.add
+
+    Telemetry telemetry;
+    public Pickup(Telemetry telemetry){
+        this.telemetry=telemetry;
+        telemetry.add("Pickup initialized");
+        telemetry.update();
     }
+    public void pull() {
+        this.telemtry.add("Pickup is pulling!!");
+        this.telemetry.update();
+    }
+    public void push(){
+        this.telemtry.add("Pickup is pushing!!");
+        this.telemetry.update();
+    }
+    public void stop(){
+        this.telemetry.add("Pickup is stopped!!!");
+        this.telemetry.update();
+    }
+    public void teleopUpdate(Gamepad gamepad1, Gamepad gamepad2){
+        this.telemetry.add("updated gamepad!");
+        this.telemetry.update();
+    }
+
 }
