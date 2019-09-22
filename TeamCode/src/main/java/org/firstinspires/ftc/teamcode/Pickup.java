079cff673ebabbfb070a94acbee1cdb8fdf43f15
Kasey <kaseyicecream@gmail.com>
2019-09-22 14:43:27 -0400

    Started pickup class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pickup.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Pickup {
+    Telemetry telemetry = new Telemetry();
+    public void pull() {
+        telemetry.add
+    }
+}
