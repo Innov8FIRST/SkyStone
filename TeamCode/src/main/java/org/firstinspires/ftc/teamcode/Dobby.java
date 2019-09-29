19a0bb2af3854d72e92cb5e7d6bc378f55552052
maya <purplecoolness101@gmail.com>
2019-09-29 14:45:13 -0400

    added hardware map as parameter for Dobby

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dobby.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dobby.java
@@ -1,18 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Dobby {
     Telemetry telemetry;
     String DOBBY_CAPTION = "Dobby Status";
-    public Dobby(Telemetry telemetry){
+    DriveTrain driveTrain;
+    Pickup pickup;
+    Lift lift;
+    HardwareInnov8Dobby robot = new HardwareInnov8Dobby();
+    public Dobby(Telemetry telemetry, HardwareMap hwmap){
+        this.initVuforia();
+        driveTrain = new DriveTrain(this.telemetry);
+        pickup = new Pickup(this.telemetry);
+        lift = new Lift(this.telemetry);
         this.telemetry = telemetry;
         this.telemetry.addData(DOBBY_CAPTION, "Dobby is ready to go");
         this.telemetry.update();
+        robot.init(hwmap);
     }
     public void initVuforia(){
         this.telemetry.addData(DOBBY_CAPTION,"Dobby initialized vuforia");
         this.telemetry.update();
+    }
+    public double getConfi(){
+        this.telemetry.addData(DOBBY_CAPTION, "Dobby is getting confi");
+        double confi = 0.98;
+        return confi;
     }
     public void stop(){
         this.telemetry.addData(DOBBY_CAPTION,"Dobby is free");
@@ -22,8 +38,15 @@ public class Dobby {
         this.telemetry.addData(DOBBY_CAPTION,"Dobby is teleop-ing");
         this.telemetry.update();
     }
-    public void autonomous(){
-        this.telemetry.addData(DOBBY_CAPTION,"Dobby is doing the autonomous");
+    public void autonomous() {
+        this.telemetry.addData(DOBBY_CAPTION, "Dobby is doing the autonomous");
+        driveTrain.goForward(20.00); // assuming robot is 18" long & camera is on front of robot
+        int blockNum = 1;
+        while(getConfi()<0.8 && blockNum < 6){
+            driveTrain.goLeft(8.0);
+            blockNum++;
+        }
+
         this.telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Innov8Autonomous_Dobby.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Innov8Autonomous_Dobby.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Innov8Teleop_Dobby", group = "Dobby")
-public class Innov8Teleop_Dobby extends LinearOpMode {
-
+@Autonomous(name = "Innov8Teleop_Dobby", group = "Dobby")
+public class Innov8Autonomous_Dobby extends LinearOpMode
+{
     @Override
     public void runOpMode() throws InterruptedException {
         Dobby dobby = new Dobby(telemetry, hardwareMap);
         waitForStart();
-        dobby.teleop();
+        dobby.autonomous();
     }
+
 }
